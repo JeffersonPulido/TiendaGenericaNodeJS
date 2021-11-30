1a3bb991abf76477486fee8f2c4a92783b88512b
Jefferson Pulido <jeffersonpulido55@gmail.com>
2021-11-30 10:21:33 -0500

    V1.6

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const csv = require("csvtojson");
+const bodyParser = require('body-parser');
 const multer = require('multer');
+const csvModel = require('./models/csv');
 //CONECT BD
 const { url } = require('./config/database.js');
 mongoose.connect(url)
@@ -23,7 +25,7 @@ app.use(express.static(__dirname + '/public'));
 //MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 //ROUTES
 app.use('/', indexRoutes);
 
@@ -35,48 +37,4 @@ app.listen(app.get('port'), () => {
 //404
 app.use((req, res, next) => {
     res.status(404).render("404", { titulo: "Página 404" });
-});
-
-//
-//UPLOAD CSV
-const storage = multer.diskStorage({
-    destination:(req,file,cb) => {
-        cb(null, './public/uploads');
-    },
-    filename:(req,file,cb) => {
-        cb(null,file.originalname);
-    }
-});
-
-const uploads = multer({storage:storage});
-var temp;
-
-app.post('/upCsv', uploads.single('csv'),(req,res) => {
-    //CONVERTIR CSV A JSON
-    csv()
-    .fromFile(req.file.path)
-    .then((jsonObj) => {
-        console.log(jsonObj);
-        for(var x=0; x<jsonObj; x++){
-            temp = parseFloat(jsonObj[x].codigo_producto)
-            jsonObj[x].codigo_producto = temp;
-            temp = parseFloat(jsonObj[x].nombre_producto)
-            jsonObj[x].nombre_producto = temp;
-            temp = parseFloat(jsonObj[x].nitproveedor)
-            jsonObj[x].nitproveedor = temp;
-            temp = parseFloat(jsonObj[x].precio_compra)
-            jsonObj[x].precio_compra = temp;
-            temp = parseFloat(jsonObj[x].ivacompra)
-            jsonObj[x].ivacompra = temp;
-            temp = parseFloat(jsonObj[x].precio_venta)
-            jsonObj[x].precio_venta = temp;
-        }
-        csvModel.insertMany(jsonObj, (err, data) => {
-            if(err){
-                console.log(err);
-            }else{
-                res.redirect('/productos')
-            }
-        });
-    });
 });
--- src/models/csv.js
+++ src/models/csv.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
-
-const csvSchema = mongoose.Schema({
-    codigo_producto:{
-        type:Number
-    },
-    nombre_producto:{
-        type:String
-    },
-    nitproveedor:{
-        type:Number
-    },
-    precio_compra:{
-        type:Number
-    },
-    ivacompra:{
-        type:Number
-    },
-    precio_venta:{
-        type:Number
-    }
-});
-
-module.exports = mongoose.model('db_productos', csvSchema);
+var mongoose  =  require('mongoose');  
+   
+var csvSchema = new mongoose.Schema({  
+    codigo_producto:{  
+        type:Number  
+    },  
+    nombre_producto:{  
+        type:String  
+    },  
+    nitproveedor:{  
+        type:Number  
+    },  
+    precio_compra:{  
+        type:Number  
+    },  
+    ivacompra:{  
+        type:Number  
+    },  
+    precio_venta:{  
+        type:Number  
+    } 
+});  
+   
+module.exports = mongoose.model('db_productos',csvSchema);  
